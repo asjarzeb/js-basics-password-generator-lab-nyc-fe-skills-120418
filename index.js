07537d7439348528626ed39b21d863ea5016ff83
AJ Jarzebiak <asjarzeb@gmail.com>
2019-03-28 00:47:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ window.addEventListener("load", () => {
   //start your code here
   
 const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
-  
+
+document.addEventListener('submit', onSubmit)
   
   
   
