9e70b261aaeee09305af5eb8a186b5cf3319d67f
AJ Jarzebiak <asjarzeb@gmail.com>
2019-03-28 00:52:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
 function onSubmit(event) {
   console.log('hi')
   event.preventDefault()
+  const input = document.getElementById('password-length')
+  console.log(input.value)
   
   
 }
